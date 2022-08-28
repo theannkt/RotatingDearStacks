21d83489a8f52acfc1a251d6e2ecd7bbaa745994
Thea N <>
2022-08-28 22:48:21 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int x, y, z; 
+  cout << "You had me at Hello World" << endl;
+  cout << "Please enter 2 integers" << endl; 
+  cin >> x >> y; 
+  z = x * x * y + y; 
+  cout << "Z is " << z << endl;
+  return -5;
+}
